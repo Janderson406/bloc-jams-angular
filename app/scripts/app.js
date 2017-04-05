38e8fe8f0f3dd5c40a963e9d855d31429eddde99
Jon Anderson <jondanderson@gmail.com>
2017-04-04 19:00:36 -0600

    Set up routing and states

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,29 @@
-angular.module('blocJams', []);
+ (function() {
+     function config($stateProvider, $locationProvider) {
+         $locationProvider
+             .html5Mode({
+                 enabled: true,     // setting the html5Mode method's enabled property to true, the hashbang URLs are disabled
+                 requireBase: false // avoid a common $location error.
+             }); 
+
+         $stateProvider
+             .state('landing', {
+                 url: '/',
+                 templateUrl: '/templates/landing.html'
+             })
+         // $stateProvider.state(stateName ,<- unique string that identifies a state , stateConfig <- object that defines specific properties of the state)
+         // <ui-view> directive in the global file (index.html) will load the template associated with the landing state.
+             .state('album', {
+                 url: '/album',
+                 templateUrl: '/templates/album.html'
+             });     
+     
+     }
+ 
+     angular
+         .module('blocJams', ['ui.router'])
+         .config(config);
+ })();
+
+//array = list of external modules that blocJams depends on, known as dependency injection
+//To make sure the providers are accessible throughout the application, inject them using the config block on the application's root module.
