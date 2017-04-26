e127f821f9b1bcea9d12e8f1106afd7c138ae200
Jon Anderson <jondanderson@gmail.com>
2017-04-25 23:10:59 -0600

    Create SongPlayer service with play and pause methods

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,13 @@
  (function() {
-     function AlbumCtrl(Fixtures) {
+     function AlbumCtrl(Fixtures, SongPlayer) {
         this.albumData = Fixtures.getAlbum(); //AlbumCtrl uses Fixtures's getAlbum() method to get the albumPicasso object
+        this.songPlayer = SongPlayer; //property holds the S.P. service and makes the service accessible within the Album view
      }
 
      angular
          .module('blocJams')
-         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]); //add Fixtures to AlbumCtrl's array of dependencies
+         .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]); //add Fixtures and SongPlayer to AlbumCtrl's array of dependencies
  })();
+
+//To use the SongPlayer service, we need to decide where to inject it as a dependency.
+//Since we'll play music from the Album view, inject the service into AlbumCtrl:
